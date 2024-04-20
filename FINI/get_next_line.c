44af4c90564a4b8fd37fc00e0d436257511bb320
Your Name <mabril@c1r6p2.42quebec.com>
2024-04-20 18:50:15 -0400

    hello fut

---
+++ FINI/get_next_line.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabril <mabril@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/20 18:20:37 by mabril            #+#    #+#             */
+/*   Updated: 2024/04/20 18:49:31 by mabril           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "get_next_line.h"
+
+char *ft_read_line(int fd, char *lin)
+{
+	char buff[BUFFER_SIZE + 1];
+	char *tempo;
+	int flag;
+	
+	flag = 0;
+	tempo = NULL;
+	while (2)
+	{	
+		flag = read(fd, buff, BUFFER_SIZE);
+		buff[flag] = '\0';
+		if (lin)
+		{
+			tempo = ft_strjoin(lin, buff);
+			if (ft_strchr(tempo, '\n') || flag < BUFFER_SIZE )
+				return (tempo);
+		}
+		else if (tempo)
+		{
+			lin = ft_strjoin(tempo, buff);
+			if (ft_strchr(lin, '\n'))
+				return(lin);
+		}
+		else
+			tempo = ft_strdup(buff);
+	}
+}
+
+char *get_next_line(int fd)
+{
+	char *tem;
+	char *line;
+	static char *stock;
+	
+	stock  = ft_read_line(fd, stock);
+	tem = ft_strdup(stock);
+	line = ft_strdup_n(stock);
+	if (stock)
+		free(stock);
+	if (ft_strchr(tem, '\n'))
+		stock = ft_strdup_apr_n(tem);
+	if (tem)
+		free(tem);
+	return (line);
+}
+
+int main()
+{
+	int fd = open("file.txt", O_RDWR);
+	char *str;
+	// if (str)
+	// {
+	// 	str = gnl(fd);
+	// 	printf("%s", str);
+	// 	free(str);
+	// }	
+	str = get_next_line(fd);
+	printf("%s", str);
+	free(str);
+	// str = get_next_line(fd);
+	// printf("%s", str);
+	// free(str);
+	// str = get_next_line(fd);
+	// printf("%s", str);
+	// free(str);
+	// while ((str = gnl(fd)) != NULL)
+	// {
+	// 	printf("%s", str);
+	// 	free(str);
+	// }
+	
+    close(fd);
+	return (0);
+}
---
+++ FINI/get_next_line_utils.c
@@ -0,0 +1,154 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabril <mabril@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/20 18:14:43 by mabril            #+#    #+#             */
+/*   Updated: 2024/04/20 18:47:58 by mabril           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int i;
+	char *t;
+
+	i  = 0;
+	t = (char *)s;
+	while (*t)
+	{
+		if (*t == (char)c)
+			return (++t);
+		t++;
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	int		len;
+	char	*copie;
+	int		i;
+
+	len = 0;
+	i = 0;
+	while (s1[len])
+		len++;
+	copie = malloc(sizeof(char) * (len + 1));
+	if (!copie)
+		return (0);
+	while (s1[i])
+	{
+		copie[i] = s1[i];
+		i ++;
+	}
+	copie[i] = '\0';
+	return (copie);
+}
+
+int	ft_strlen(const char	*s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*new_str;
+	int		i;
+	int		j;
+
+	new_str = malloc(sizeof(char) * ((ft_strlen(s1) + ft_strlen(s2)) + 1));
+	if (!new_str)
+		return (0);
+	i = 0;
+	while (s1[i])
+	{
+		new_str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		new_str[i + j] = s2[j];
+		j++;
+	}
+	if (s1)
+		free(s1);
+	s1 = NULL;
+	new_str[i + j] = '\0';
+	return (new_str);
+}
+// char  *ft_strcpy(char *dst, const char *src)
+// {
+//     size_t  len;
+    
+//     len = 0;
+//     len = ft_strlen(src);
+//     if (!src)
+//         return (0);
+//     while (*src != '\0')
+//         *dst++ = *src++;
+//     *dst = '\0';
+//     return (dst);
+// }
+
+
+char	*ft_strdup_n(const char *s1)
+{
+	int		len;
+	char	*copie;
+	int		i;
+
+	len = 0;
+	i = 0;
+	while (s1[len] != '\n' && s1[len + 1] != '\0')
+		len++;
+	copie = malloc(sizeof(char) * (len + 1));
+	if (!copie)
+		return (0);
+	while (i <= len)
+	{
+		copie[i] = s1[i];
+		i ++;
+	}
+	copie[i] = '\0';
+	return (copie);
+}
+
+char	*ft_strdup_apr_n(const char *s1)
+{
+	int		len;
+	char	*copie;
+	int		i;
+
+	len = 0;
+	i = 0;
+	while (s1[len + 1] != '\0')
+		len++;
+	while (s1[i] != '\n')
+		i++;
+	copie = malloc(sizeof(char) * ((len - i) + 1));
+	if (!copie)
+		return (0);
+	len = 0;
+	while (s1[i+1] != '\0')
+	{
+		copie[len] = s1[i + 1];
+		len++;
+		i++;
+	}
+	copie[len] = '\0';
+	return (copie);
+}
